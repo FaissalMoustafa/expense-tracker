4ab8096670f494d91b2bd098768f53978fdb7713
Faissal Moustafa <faissalmoustafa@gmail.com>
2023-07-19 08:02:34 +0300

    adding table

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ function newExpense(exp,amount) {
   }
 
 function addExpense() {
-    const exp_name = $("#exp-name");
-    const exp_amount =$("#exp-amount")
+    const exp_name = $("#exp-name").val().trim();
+    const exp_amount =parseFloat($("#exp-amount").val());
     const exp_list = $("#exp-list");
 
-    if (exp_name.val().trim() === "") return;
+    if (exp_name=== ""|| isNaN(exp_amount) || exp_amount <= 0) return;
 
     const new_exp = $(newExpense(exp_name.val(),exp_amount.val()))
 
@@ -36,4 +36,23 @@ $(document).ready(function () {
     //   if the user pressed enter instead of the Add button
     }
     )
+
+    const exp_list_items = $("#exp-list li"); // gets all list items
+    const exp_table = $("<table></table>"); // Create a new table element
+    const header_row = $("<th>List Items</th>");
+    exp_table.append(header_row);
+
+    // Add expenses as rows to the table
+    exp_list_items.each(function() {
+      const new_exp = $(this).text();
+      const new_entries = $("<tr></tr>");
+      new_entries.append($("<td></td>").text(new_exp));
+      exp_table.append(new_entries);
+    });
+
+     // Hide the expenses list
+     $("#exp-list").hide();
+
+     // Append the table to the "table-container" div
+     $("#exp-table").append(exp_table);
   });
