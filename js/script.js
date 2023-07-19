f386aefdf6e804355ad5df211b9357b74a810236
Faissal Moustafa <faissalmoustafa@gmail.com>
2023-07-19 06:32:58 +0300

    JS file dest. fix

--- js/script.js
+++ js/script.js
@@ -2,20 +2,38 @@ function newExpense(exp,amount) {
     return `<li>
       <span class="delete">&#128465</span>
       <span class="text">${exp}</span>
-      
+      <span class="text">${amount}</span>
       <input type="text" style="display: none;"/>
     </li>`
   }
 
 function addExpense() {
-const expInput = $("#exp-name");
-const todoList = $("#todo-list");
+    const exp_name = $("#exp-name");
+    const exp_amount =$("#exp-amount")
+    const exp_list = $("#exp-list");
 
-if (todoInput.val().trim() === "") return;
+    if (exp_name.val().trim() === "") return;
 
-const todoItem = $(todoItemElement(todoInput.val()))
+    const new_exp = $(newExpense(exp_name.val(),exp_amount.val()))
 
-//todo: remove
-todoItem.find(".remove").click(function () {
-    todoItem.remove()
-})}
+    //expense: delete
+    new_exp.find(".delete").click(function () {
+        new_exp.remove()
+    })
+    exp_list.append(new_exp)
+    exp_name.val("") 
+    exp_amount.val("")
+}
+
+$(document).ready(function () {
+    const addButton = $("#add-button");
+    addButton.click(addExpense);
+  
+    $("#exp-name").keyup(function (event) {  
+      if (event.keyCode === 13) {
+        addExpense()
+      }
+    //   if the user pressed enter instead of the Add button
+    }
+    )
+  });
